a9c51540db79fd73e016c47b6b5ac69b45db98f0
dabii9870 <davereub99@gmail.com>
2026-04-20 12:13:22 +0530

    lerning frames

---
+++ learning_frames.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+root = Tk()
+root.title('Learning Frames')
+root.geometry('400x400')
+#page 1
+
+def show_login_page():
+    signup_page.pack_forget()
+    login_page.pack(fill='both', expand=True)
+def show_sign_up_page():
+    login_page.pack_forget()
+    signup_page.pack(fill='both', expand=True)
+
+login_page = Frame(root, bg="#D31B1B")
+label = Label(login_page, text='login_page')
+button1 = Button(login_page, text='go to sign up page', command=show_sign_up_page)
+label.pack()
+button1.pack()
+
+
+signup_page = Frame(root, bg="#3B035C")
+label1 = Label(signup_page, text='signupage')
+button2 = Button(signup_page, text='go to login page', command=show_login_page)
+label1.pack()
+button2.pack()
+
+
+login_page.pack(fill='both', expand=True)
+
+
+root.mainloop()
